771a115c3710c2e68d5c3d02994f68876c0d035b
Kaspars <smilga.kaspars@gmail.com>
2017-09-10 21:50:27 +0300

    changes

--- settings.el
+++ settings.el
@@ -1,10 +1,7 @@
-(require 'org)
-(org-babel-load-file
- (expand-file-name "settings.org"
-                   user-emacs-directory))
 
 ;;Don`t show startup message
 (setq inhibit-startup-message t)
+
 
 ;;Take off toolbar
 (tool-bar-mode -1)
@@ -17,7 +14,7 @@
 (require 'package)
 (setq pacakge-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 
@@ -127,7 +124,7 @@
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-safe-themes
    (quote
-    ("b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "1d079355c721b517fdc9891f0fda927fe3f87288f2e6cc3b8566655a64ca5453" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "6f11ad991da959fa8de046f7f8271b22d3a97ee7b6eca62c81d5a917790a45d9" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" default)))
+    ("1d079355c721b517fdc9891f0fda927fe3f87288f2e6cc3b8566655a64ca5453" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" "d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" "6f11ad991da959fa8de046f7f8271b22d3a97ee7b6eca62c81d5a917790a45d9" "b81bfd85aed18e4341dbf4d461ed42d75ec78820a60ce86730fc17fc949389b2" default)))
  '(package-selected-packages
    (quote
     (evil evil-mode color-theme color-themes auto-complete counsel swiper ace-window which-key try use-package))))
@@ -137,3 +134,26 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+
+(use-package general :ensure t
+  :config
+  (general-evil-setup t)
+
+  (general-define-key
+   :states '(normal insert emacs)
+   :prefix "C-SPC"
+   :non-normal-prefix "C-SPC"
+   "l" '(avy-goto-line)
+   "a" 'align-regexp
+   )
+
+  (general-define-key
+   :states '(normal motion insert emacs)
+   :prefix "SPC"
+   "ar" '(ranger :which-key "call ranger")
+   "g"  '(:ignore t :which-key "Git")
+   "gs" '(magit-status :which-key "git status")
+   "f" '(find-file :which-key "find file")
+   "w" '(save-buffer)
+   )
+)
