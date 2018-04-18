1e658333cac9cd46f7f71b7818e2e8b859683a6f
Kaspars Smilga <smilga.kaspars@gmail.com>
2018-04-18 10:59:14 +0300

    refacture

---
+++ custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (counsel which-key powerline redis evil-lion auto-highlight-symbol evil-commentary smart-tab json-mode rainbow-delimiters multiple-cursors git-gutter emmet-mode evil go-autocomplete go-eldoc go-mode exec-path-from-shell web-mode js2-mode js-import highlight-numbers hl-todo php-auto-yasnippets yasnippet-snippets yasnippet imenu-list auto-complete flycheck dumb-jump general helm-ag helm projectile autopair neotree linum-relative use-package try doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(neo-vc-added-face ((t (:foreground "lime green"))))
+ '(neo-vc-edited-face ((t (:foreground "gold")))))
