1c8980e79c24465623a67bbc8fdb761a38786c81
Kaspars Smilga <smilga.kaspars@gmail.com>
2018-04-19 17:17:53 +0300

    company statistics

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
  '(git-gutter:window-width 1)
  '(package-selected-packages
    (quote
-    (company-go company-statistics company-web company-web-html company company-mode ivy rainbow-mode php-mode counsel which-key powerline redis evil-lion auto-highlight-symbol evil-commentary smart-tab json-mode rainbow-delimiters multiple-cursors git-gutter emmet-mode evil go-autocomplete go-eldoc go-mode exec-path-from-shell web-mode js2-mode js-import highlight-numbers hl-todo php-auto-yasnippets yasnippet-snippets yasnippet imenu-list auto-complete flycheck dumb-jump general helm-ag helm projectile autopair neotree linum-relative use-package try doom-themes))))
+    (evil-magit magit evil-surround evil-matchit evil-mathcit company-go company-statistics company-web company-web-html company company-mode ivy rainbow-mode php-mode counsel which-key powerline redis evil-lion auto-highlight-symbol evil-commentary smart-tab json-mode rainbow-delimiters multiple-cursors git-gutter emmet-mode evil go-autocomplete go-eldoc go-mode exec-path-from-shell web-mode js2-mode js-import highlight-numbers hl-todo php-auto-yasnippets yasnippet-snippets yasnippet imenu-list auto-complete flycheck dumb-jump general helm-ag helm projectile autopair neotree linum-relative use-package try doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
