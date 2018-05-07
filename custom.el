66416b09422da26935b1c9b3b20ea3b8f9c3e2ed
Kaspars <kaspars.smilga@protonmail.com>
2018-05-07 22:28:22 +0300

    added system monitor package

--- custom.el
+++ custom.el
@@ -11,6 +11,7 @@
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "55ed02951e7b458e4cd18837eefa1956884c9afd22bb514f697fd1d2d1abb3d3" "bea5fd3610ed135e6ecc35bf8a9c27277d50336455dbdd2969809f7d7c1f7d79" "ef1e992ef341e86397b39ee6b41c1368e1b33d45b0848feac6a8e8d5753daa67" "bbea3143fe920d2738e73a042d1edcc0e0c1e9726a757dc0b51ca8d5ee825255" "53d1bb57dadafbdebb5fbd1a57c2d53d2b4db617f3e0e05849e78a4f78df3a1b" "c5d320f0b5b354b2be511882fc90def1d32ac5d38cccc8c68eab60a62d1621f2" "4e21fb654406f11ab2a628c47c1cbe53bab645d32f2c807ee2295436f09103c6" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "bd23e5e571f9b951eb79941ba3927fb493c26463654add2a53f4fb0de72ef08b" "55d31108a7dc4a268a1432cd60a7558824223684afecefa6fae327212c40f8d3" "1884b12dbfa952664f1119bd6d89aaee77b35fbe3c3539381fbdc84a5bd907ef" default)))
  '(fci-rule-color "#5c5e5e")
+ '(flx-ido-mode t)
  '(git-gutter:added-sign "|")
  '(git-gutter:deleted-sign "|")
  '(git-gutter:modified-sign "|")
@@ -25,12 +26,13 @@
     ("#336c6c" "#205070" "#0f2050" "#806080" "#401440" "#6c1f1c" "#6b400c" "#23733c")))
  '(package-selected-packages
    (quote
-    (telephone-line flx-ido autumn-light-theme theme-changer evil-magit magit evil-surround evil-matchit evil-mathcit company-go company-statistics company-web company-web-html company company-mode ivy rainbow-mode php-mode counsel which-key powerline redis evil-lion auto-highlight-symbol evil-commentary smart-tab json-mode rainbow-delimiters multiple-cursors git-gutter emmet-mode evil go-autocomplete go-eldoc go-mode exec-path-from-shell web-mode js2-mode js-import highlight-numbers hl-todo php-auto-yasnippets yasnippet-snippets yasnippet imenu-list auto-complete flycheck dumb-jump general helm-ag helm projectile autopair neotree linum-relative use-package try doom-themes)))
+    (symon telephone-line flx-ido autumn-light-theme theme-changer evil-magit magit evil-surround evil-matchit evil-mathcit company-go company-statistics company-web company-web-html company company-mode ivy rainbow-mode php-mode counsel which-key powerline redis evil-lion auto-highlight-symbol evil-commentary smart-tab json-mode rainbow-delimiters multiple-cursors git-gutter emmet-mode evil go-autocomplete go-eldoc go-mode exec-path-from-shell web-mode js2-mode js-import highlight-numbers hl-todo php-auto-yasnippets yasnippet-snippets yasnippet imenu-list auto-complete flycheck dumb-jump general helm-ag helm projectile autopair neotree linum-relative use-package try doom-themes)))
  '(pdf-view-midnight-colors (quote ("#232333" . "#c7c7c7")))
  '(sml/active-background-color "#98ece8")
  '(sml/active-foreground-color "#424242")
  '(sml/inactive-background-color "#4fa8a8")
  '(sml/inactive-foreground-color "#424242")
+ '(telephone-line-mode t)
  '(vc-annotate-background "#0d0d0d")
  '(vc-annotate-color-map
    (list
